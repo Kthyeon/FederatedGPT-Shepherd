009aa40852238d9e0183299f472c90093c3aeee5
Kthyeon <potter32@kaist.ac.kr>
2023-06-21 10:07:02 +0900

    Update - 4 client setting with dolly dataset

---
+++ scripts/4client_dolly.sh
@@ -0,0 +1,84 @@
+# all for lora
+python main.py --global_model 'chavinlo/alpaca-native'\
+      --data_path  "./data" \
+      --output_dir  './lora-shepherd-7b-all-r8-4cli/'\
+      --device_map 'auto' \
+      --num_communication_rounds 20 \
+      --num_clients  4 \
+      --client_selection_frac 1.0 \
+      --local_num_epochs  2 \
+      --local_batch_size  52 \
+      --local_micro_batch_size 26 \
+      --local_learning_rate 0.0003 \
+      --lora_r 8 \
+      --lora_target_modules='[q_proj,k_proj,v_proj,o_proj]' \
+      --train_on_inputs \
+      --group_by_length
+
+# all for lora
+python main.py 1 --global_model 'chavinlo/alpaca-native'\
+      --data_path  "./data" \
+      --output_dir  './lora-shepherd-7b-q-r8-4cli/'\
+      --device_map 'auto' \
+      --num_communication_rounds 20 \
+      --num_clients  4 \
+      --client_selection_frac 1.0 \
+      --local_num_epochs  2 \
+      --local_batch_size  52 \
+      --local_micro_batch_size 26 \
+      --local_learning_rate 0.0003 \
+      --lora_r 8 \
+      --lora_target_modules='[q_proj]' \
+      --train_on_inputs \
+      --group_by_length 
+
+# all for lora
+python main.py --global_model 'chavinlo/alpaca-native'\
+      --data_path  "./data" \
+      --output_dir  './lora-shepherd-7b-k-r8-4cli/'\
+      --device_map 'auto' \
+      --num_communication_rounds 20 \
+      --num_clients  4 \
+      --client_selection_frac 1.0 \
+      --local_num_epochs  2 \
+      --local_batch_size  52 \
+      --local_micro_batch_size 26 \
+      --local_learning_rate 0.0003 \
+      --lora_r 8 \
+      --lora_target_modules='[k_proj]' \
+      --train_on_inputs \
+      --group_by_length 
+
+# all for lora
+python main.py --global_model 'chavinlo/alpaca-native'\
+      --data_path  "./data" \
+      --output_dir  './lora-shepherd-7b-v-r8-4cli/'\
+      --device_map 'auto' \
+      --num_communication_rounds 20 \
+      --num_clients  4 \
+      --client_selection_frac 1.0 \
+      --local_num_epochs  2 \
+      --local_batch_size  52 \
+      --local_micro_batch_size 26 \
+      --local_learning_rate 0.0003 \
+      --lora_r 8 \
+      --lora_target_modules='[v_proj]' \
+      --train_on_inputs \
+      --group_by_length 
+
+# all for lora
+python main.py --global_model 'chavinlo/alpaca-native'\
+      --data_path  "./data" \
+      --output_dir  './lora-shepherd-7b-o-r8-4cli/'\
+      --device_map 'auto' \
+      --num_communication_rounds 20 \
+      --num_clients  4 \
+      --client_selection_frac 1.0 \
+      --local_num_epochs  2 \
+      --local_batch_size  52 \
+      --local_micro_batch_size 26 \
+      --local_learning_rate 0.0003 \
+      --lora_r 8 \
+      --lora_target_modules='[o_proj]' \
+      --train_on_inputs \
+      --group_by_length  
--- scripts/preparation.sh
+++ scripts/preparation.sh
@@ -1,13 +1,13 @@
 # conda create --name fedit python=3.9
 # conda update --all
 # pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
-pip install -r requirements.txt
+# pip install -r requirements.txt
 
 # Data_preparation
-num_client=2 # The number of clients
+num_client=4 # The number of clients
 diff_quantity=0 # Whether clients have different amounts of data
 num_shards_per_clients=1 
-python client_data_allocation.py $num_client $diff_quantity
+python client_data_allocation.py $num_client $diff_quantity $num_shards_per_clients
 
 
 ##### Example 1 with two clients
@@ -27,3 +27,32 @@ python client_data_allocation.py $num_client $diff_quantity
 # creative_writing     701
 # Name: category, dtype: int64 ['brainstorming' 'classification' 'closed_qa' 'creative_writing'
 #  'general_qa'] 7468
+
+
+##### Example 5 with four clients
+#  Generating the local training dataset of Client_0
+# category
+# classification    1976
+# brainstorming     1758
+# Name: count, dtype: int64 ['brainstorming' 'classification'] 3734
+
+#  Generating the local training dataset of Client_1
+# category
+# open_qa          2480
+# summarization    1253
+# Name: count, dtype: int64 ['open_qa' 'summarization'] 3733
+
+#  Generating the local training dataset of Client_2
+# category
+# information_extraction    1502
+# open_qa                   1121
+# general_qa                1111
+# Name: count, dtype: int64 ['general_qa' 'information_extraction' 'open_qa'] 3734
+
+#  Generating the local training dataset of Client_3
+# category
+# closed_qa           1813
+# general_qa          1070
+# creative_writing     701
+# classification       150
+# Name: count, dtype: int64 ['classification' 'closed_qa' 'creative_writing' 'general_qa'] 3734
