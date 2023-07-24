4bde035791021b1a46c97b8fcd3c3384b0857ec4
potter32@kaist.ac.kr <potter32@kaist.ac.kr>
2023-07-24 05:25:33 +0000

    Update - vicuna fedsira

--- scripts/vicuna/8client_vicuna13b.sh
+++ scripts/vicuna/8client_vicuna13b.sh
@@ -1,7 +1,41 @@
+# # all for lora
+# python main.py --global_model 'lmsys/vicuna-13b-v1.3'\
+#       --data_path  "./data" \
+#       --output_dir  '/data/taehyeon/models/vicuna13b-all-r8-8cli/'\
+#       --device_map 'auto' \
+#       --num_communication_rounds 20 \
+#       --num_clients  8 \
+#       --client_selection_frac 1.0 \
+#       --local_num_epochs  2 \
+#       --local_batch_size  60 \
+#       --local_micro_batch_size 60 \
+#       --local_learning_rate 0.0003 \
+#       --lora_r 8 \
+#       --lora_target_modules='[q_proj,k_proj,v_proj,o_proj]' \
+#       --train_on_inputs \
+#       --group_by_length
+
+# # all for lora
+# python main.py --global_model 'lmsys/vicuna-13b-v1.3'\
+#       --data_path  "./data" \
+#       --output_dir  '/data/taehyeon/models/vicuna13b-q-r8-8cli/'\
+#       --device_map 'auto' \
+#       --num_communication_rounds 20 \
+#       --num_clients  8 \
+#       --client_selection_frac 1.0 \
+#       --local_num_epochs  2 \
+#       --local_batch_size  60 \
+#       --local_micro_batch_size 60 \
+#       --local_learning_rate 0.0003 \
+#       --lora_r 8 \
+#       --lora_target_modules='[q_proj]' \
+#       --train_on_inputs \
+#       --group_by_length
+
 # all for lora
 python main.py --global_model 'lmsys/vicuna-13b-v1.3'\
       --data_path  "./data" \
-      --output_dir  '/data/taehyeon/models/vicuna13b-all-r8-8cli/'\
+      --output_dir  './vicuna13b-all-r8-8cli-fedsira/'\
       --device_map 'auto' \
       --num_communication_rounds 20 \
       --num_clients  8 \
@@ -13,12 +47,13 @@ python main.py --global_model 'lmsys/vicuna-13b-v1.3'\
       --lora_r 8 \
       --lora_target_modules='[q_proj,k_proj,v_proj,o_proj]' \
       --train_on_inputs \
-      --group_by_length
+      --group_by_length \
+      --aggregator "fedsira"
 
 # all for lora
 python main.py --global_model 'lmsys/vicuna-13b-v1.3'\
       --data_path  "./data" \
-      --output_dir  '/data/taehyeon/models/vicuna13b-q-r8-8cli/'\
+      --output_dir  './vicuna13b-q-r8-8cli-fedsira/'\
       --device_map 'auto' \
       --num_communication_rounds 20 \
       --num_clients  8 \
@@ -30,7 +65,9 @@ python main.py --global_model 'lmsys/vicuna-13b-v1.3'\
       --lora_r 8 \
       --lora_target_modules='[q_proj]' \
       --train_on_inputs \
-      --group_by_length 
+      --group_by_length \
+      --aggregator "fedsira"
+ 
 
 # # all for lora
 # python main.py --global_model 'chavinlo/alpaca-native'\
